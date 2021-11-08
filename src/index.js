8b1be5ee0e825383ea07b77f133bf64b3383e997
kirill-herman <kirill.herman.public@gmail.com>
2021-11-08 20:43:43 +0300

    Learn React: Part 1

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const elem = <h2 className='test'>Hello World!</h2>;
+
+//same
+// const elem = React.createElement('h2', null || {className: 'test'}, 'Hello World!');
+
+// const element = {
+//   type: 'h2',
+//   props: {
+//     className: 'test',
+//     children: 'Hello World!'
+//   }
+// };
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  elem,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
